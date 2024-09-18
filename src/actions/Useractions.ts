e7503c84896c04b1f37793621dba9e76fbcad437
tanmay kumar chaurasia <kuamrchaurasiatanmay@gmail.com>
2024-09-18 20:16:48 +0530

    building

---
+++ src/actions/Useractions.ts
@@ -0,0 +1,13 @@
+"use server"
+
+import Razorpay from "razorpay";
+import PaymentModel from "@/models/paymentModel";
+import { dbconnect } from "@/db/db";
+import userModel from "@/models/userModel";
+
+
+
+
+
+
+
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -6,21 +6,13 @@ import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
 import axios from "axios"; // Added axios import
 import toast from "react-hot-toast";
-import { signIn } from "next-auth/react";
-import { useSession, signOut } from "next-auth/react";
+
 import { IconBrandGithub, IconBrandGoogle } from "@tabler/icons-react";
 
 export function SignupFormDemo() {
-  const { data: session }: any = useSession();
+
   const router = useRouter();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+
   const [user, setUser] = useState({
     username: "",
     email: "",
@@ -120,7 +112,6 @@ export function SignupFormDemo() {
 
         <div className="flex flex-col space-y-4">
           <button
-            onClick={() => signIn("github")}
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
             type="button"
             // Changed to button (not submitting the form)
